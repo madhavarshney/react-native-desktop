be40223fcb820d12efd6529addae4fab34cf3adf
Madhav Varshney <madhavarshney@gmail.com>
2018-11-26 16:56:09 -0800

    Desktop out-of-tree platform support - clean up code base

---
+++ Libraries/react-native-desktop/react-native-desktop-implementation.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2017-present, Status Research and Development GmbH.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow
+ */
+
+'use strict';
+
+const ReactNativeDesktop = {
+  get PickerUbuntu() {
+    return require('PickerUbuntu');
+  }
+};
+
+module.exports = ReactNativeDesktop;
